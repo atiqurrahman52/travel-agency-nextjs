b2195ee28670db94ac6d93d06579a013e705087f
atiqurrahman52 <atiqur.rahman@intelli.global>
2022-10-23 12:49:37 +0600

    updated

--- components/shared/Logo.js
+++ components/shared/Logo.js
@@ -2,15 +2,15 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Logo = () => {
-    return (
-      <Link href="/">
-        {/* <a> */}
-          <div className="relative h-[26px] md:h-[54px] w-[84px] md:w-[176px]">
-            <Image src="/assets/images/header/logo.svg" layout="fill" alt="" />
-          </div>
-        {/* </a> */}
-      </Link>
-    );
-  };
+  return (
+    <Link href="/">
+      {/* <a> */}
+      <div className="relative h-[26px] md:h-[54px] w-[84px] md:w-[176px]">
+        <Image src="/assets/images/header/logo.svg" layout="fill" alt="" />
+      </div>
+      {/* </a> */}
+    </Link>
+  );
+};
 
-  export default Logo 
+export default Logo;
